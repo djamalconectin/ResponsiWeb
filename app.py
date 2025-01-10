ae5bab57021d31b19328af4603b206a2ddcad5e1
djamalconectin <jamalkeren099@gmail.com>
2025-01-10 22:57:12 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,27 +1,49 @@
-from flask import Flask, render_template
-from flask import request, redirect, url_for
-from mysql import connector
+import mysql.connector
+from flask import Flask, render_template, request, redirect, url_for
+from mysql.connector import Error
 
 app = Flask(__name__)
 
-#open connecton
-db = connector.connect(
-    host = "localhost",
-    user = "root",
-    passwd = '',
-    database = 'db_kuliah'
-)
+def get_connection():
+    try: 
+        connection = mysql.connector.connect(
+            host = "sne2c.h.filess.io",
+            database = "JamalTampan_figurehot",
+            port = "3307",
+            user = "JamalTampan_figurehot",
+            password = "97e0ce7eedbbcc3abf6736c4b57f93a74fa5b2c9"
+        )
+        return connection
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+        return None
 
-if db.is_connected():
-    print('open connection succescful')
+try:
+    connection = get_connection()
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("You're connected to database: ", record)
+
+except Error as e:
+    print("Error while connecting to MySQL", e)
 
 @app.route('/')
-def halaman_awal():
-    cursor = db.cursor()
-    cursor.execute("select * from db_kuliah")
-    res = cursor.fetchall()
-    cursor.close()
-    return render_template('index.html', hasil=res)
+def home():
+    connection = get_connection()
+    if connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM tbl_mhs")
+        result = cursor.fetchall()
+        cursor.close()
+        connection.close()
+    else:
+        result = []
+        print("Unable to connect to database")
+    return render_template('index.html', hasil=result)
 
 @app.route('/tambah/')
 def tambah_data():
@@ -32,18 +54,34 @@ def proses_tambah():
     nim = request.form['nim']
     nama = request.form['nama']
     asal = request.form['asal']
-    cur = db.cursor()
-    cur.execute('INSERT INTO db_kuliah  (nim, nama, asal) VALUES (%s, %s, %s)', (nim, nama, asal))
-    db.commit()
+    connection = get_connection()
+    if connection:
+        cursor = connection.cursor()
+        try:
+            cursor.execute("INSERT INTO tbl_mhs (nim, nama, asal) VALUES (%s, %s, %s)", (nim, nama, asal))
+            connection.commit()
+        except Error as e:
+            print("Error while inserting data:", e)
+        finally:
+            cursor.close()
+            connection.close()
     return redirect(url_for('halaman_awal'))
 
 @app.route('/ubah/<nim>', methods=['GET'])
 def ubah_data(nim):
-    cur = db.cursor()
-    cur.execute('select * from mahasiswa where nim=%s', (nim,))
-    res = cur.fethchall()
-    cur.close()
-    return render_template('ubah.html', hasil=res)
+    connection = get_connection()
+    result = []
+    if connection:
+        cursor = connection.cursor()
+        try:
+            cursor.execute("SELECT * FROM tbl_mhs WHERE nim = %s", (nim,))
+            result = cursor.fetchall()
+        except Error as e:
+            print("Error while fetching data:", e)
+        finally:
+            cursor.close()
+            connection.close()
+    return render_template('ubah.html', hasil=result)
 
 @app.route('/proses_ubah/', methods=['POST'])
 def proses_ubah():
@@ -51,19 +89,37 @@ def proses_ubah():
     nim = request.form['nim']
     nama = request.form['nama']
     asal = request.form['asal']
-    cur = db.cursor()
-    sql = "UPDATE mahasiswa SET nim=%s, nama=%s, asal=%s WHERE nim=%s"
-    value = (nim, nama, asal, no_mhs)
-    cur.execute(sql, value)
-    db.commit()
+    connection = get_connection()
+    if connection:
+        cursor = connection.cursor()
+        try:
+            sql = 'UPDATE tbl_mhs SET nim=%s, nama=%s, asal=%s WHERE nim=%s'
+            value = (nim, nama, asal, no_mhs)
+            cursor.execute(sql, value)
+            connection.commit()
+        except Error as e:
+            print("Error while updating data:", e)
+        finally:
+            cursor.close()
+            connection.close()
     return redirect(url_for('halaman_awal'))
 
+# Hapus Data
 @app.route('/hapus/<nim>', methods=['GET'])
 def hapus_data(nim):
-    cur = db.cursor()
-    cur.execute('DELETE from mahasiswa where nim=%s', (nim,))
-    db.commit()
+    connection = get_connection()
+    if connection:
+        cursor = connection.cursor()
+        try:
+            cursor.execute('DELETE FROM tbl_mhs WHERE nim=%s', (nim,))
+            connection.commit()
+        except Error as e:
+            print("Error while deleting data:", e)
+        finally:
+            cursor.close()
+            connection.close()
     return redirect(url_for('halaman_awal'))
 
 if __name__ == "__main__":
     app.run(debug=True)
+
